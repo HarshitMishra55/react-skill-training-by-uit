784a388cb036eff606d64e3d73cb6bd2612dbe5a
Harshit Mishra <harshit707125@gmail.com>
2026-01-27 16:09:24 +0530

    done pagination

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SimpleList from './components/SimpleList.jsx'
 import TodoList from './components/TodoList.jsx'
+import PaginatedList from './components/PaginatedList.jsx'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <marquee><h1>Harshit Mishra</h1></marquee>
       <SimpleList />
       <TodoList />
+      <PaginatedList />
     </>
   )
 }
