784e053d745c0da8320581224febadf040fc0ba9
Harshit Mishra <harshit707125@gmail.com>
2026-01-28 16:05:09 +0530

    done

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import SimpleList from './components/SimpleList.jsx'
 import TodoList from './components/TodoList.jsx'
-import { PaginatedList } from './components/PaginatedList.jsx'
 import SearchWithDebounce from './components/SearchWithDebounce.jsx'
 import InfiniteScrolling from './components/InfiniteScrolling.jsx'
-import { LiveSearchAPI } from './components/LiveSearchApi.jsx'
+import { PaginatedList } from './components/PaginatedList.jsx'
+import { LiveSearchAPI } from './components/LiveSearchAPI.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <marquee><h1>Harshit Mishra</h1></marquee>
